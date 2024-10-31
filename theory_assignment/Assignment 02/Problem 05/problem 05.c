d5f81604a869dfd7d2433dc157d02131c6088dfd
Huzaif Ali <syedhuzaifalishah987@gmail.com>
2024-10-31 04:14:43 -0700

    Update problem 05.c

--- theory_assignment/Assignment 02/Problem 05/problem 05.c
+++ theory_assignment/Assignment 02/Problem 05/problem 05.c
@@ -1,1 +1,48 @@
+#include<stdio.h>
+int main(){
+    	int n;
+	printf("\n enter array size: ");
+    	scanf("%d", &n);
+    	int arr[n], an[n];
 
+    	for(int i = 0; i < n; i++){
+		printf("\n enter value %d: ", i);
+    		scanf("%d", &arr[i]);
+    	}//end for
+
+    	int max = arr[0];
+
+    	for(int i = 0; i < n; i++){
+        	if(max < arr[i]){
+            	max = arr[i];
+        	}//end if
+    	}//end for
+
+    	for(int i = 0; i < n; i++){
+        	printf("\n value %d: ", i+1);
+        	for(int j = 0; j < arr[i]; j++){
+            		printf("*");
+        	}//end nested for
+	        an[i] = arr[i];
+        	printf(" \n");
+    	}//end for
+	printf("\n");
+
+ 	for(int i = max; i > 0; i--){
+        	for(int j = 0; j < n; j++){
+            		if(arr[j] == i){
+                		arr[j]--;
+                		printf(" *");
+            		}else{
+                		printf("  ");
+            		}//end if
+        	}//end nested for
+        	printf(" \n");
+    	}//end for
+
+    	for(int i = 0; i < n; i++){
+        	printf(" %d",an[i]);
+    	}//end for
+	printf("\n");
+
+}//end main
