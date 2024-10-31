e53be6debb02088d1656c1f4e4a6e08e86a2a8fa
Huzaif Ali <syedhuzaifalishah987@gmail.com>
2024-10-31 03:57:53 -0700

    Update problem 04.c

--- theory_assignment/Assignment 02/Problem 04/problem 04.c
+++ theory_assignment/Assignment 02/Problem 04/problem 04.c
@@ -1,1 +1,52 @@
+#include <stdio.h>
+#include <string.h>
 
+int areAnagrams(char str1[], char str2[]) {
+	int count[26] = {0}; 
+
+	for(int i = 0; str1[i] != '\0'; i++) {
+        	count[str1[i] - 'a']++; 
+        }
+    
+    	for (int i = 0; str2[i] != '\0'; i++) {
+        	count[str2[i] - 'a']--;  
+    	}
+    	for (int i = 0; i < 26; i++) {
+        	if (count[i] != 0) return 0;
+    	}
+    
+    return 1; 
+}//end function areAnagrams
+
+void groupAnagrams(char transactions[1000][100]) {
+    	int used[1000] = {0}; 
+    
+    	for (int i = 0; i < 6; i++) {
+        	if (used[i]) {
+			continue;
+		}//end if
+        	printf("[");
+        	for (int j = i; j < 6; j++) {
+            		if (!used[j] && areAnagrams(transactions[i], transactions[j])) {
+                		printf("'%s'", transactions[j]);
+                		used[j] = 1; 
+            		}//end if
+      		}//end nested for
+        	printf("] ");
+    	}//end for
+}//end groupAnagrams
+
+int main() {
+    	char transactions[1000][100];
+    
+	for (int i = 0; i < 6; i++) {
+		printf("\n enter the transaction[%d]: ", i);
+        	scanf("%s", transactions[i]);
+    	}//end for
+
+	printf("\n Grouped anagrams: ");
+	groupAnagrams(transactions);
+	printf("\n");
+    
+	return 0;
+}//end main
