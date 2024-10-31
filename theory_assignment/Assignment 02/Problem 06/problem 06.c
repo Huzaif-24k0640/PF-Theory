018e675ffa6e6d61af638fdaf41a032d453f701a
Huzaif Ali <syedhuzaifalishah987@gmail.com>
2024-10-31 04:37:00 -0700

    Update problem 06.c

--- theory_assignment/Assignment 02/Problem 06/problem 06.c
+++ theory_assignment/Assignment 02/Problem 06/problem 06.c
@@ -1,1 +1,57 @@
+#include <stdio.h>
 
+int main() {
+    int x, z = 0;
+    
+    printf("\n Enter a positive integer: ");
+    scanf("%d", &x);
+    
+    if (x < 0) {
+        printf(" Please enter a positive integer\n");
+        return 1;
+    }
+
+    int first;
+    int n;
+    
+    n = x % 4; 
+    x = x - n; 
+    first = x / 4; 
+
+    printf("\n To win pick: ");	
+
+    if (first % 2 != 0) {
+        if (n == 2) {
+            printf("one\n");
+	    z = 1;
+        } else if (n == 3) {
+            printf("two\n");
+	    z = 2;
+        } else if (n == 0) {
+            printf("four\n");
+	    z = 4;
+        } else {
+            printf("impossible to win\n");
+	    z = -1;
+        }
+    } else {
+        if (n == 1) {
+            printf("four\n");
+	    z = 4;
+        } else if (n == 2) {
+            printf("three\n");
+	    z = 3;
+        } else if (n == 3) {
+            printf("two\n");
+	    z = 2;
+        } else if (n == 0) { 
+            printf("one\n");
+	    z = 1;
+        } else {
+            printf("impossible to win\n");
+	    z = -1;
+        }
+    }
+    printf("\n Pick: %d \n", z);
+    return 0;
+}
