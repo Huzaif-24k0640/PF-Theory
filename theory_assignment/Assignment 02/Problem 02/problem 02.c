65cfaae7a56e33f5cccc4db2134ea2e6b3eaaaef
Huzaif Ali <syedhuzaifalishah987@gmail.com>
2024-10-31 02:27:38 -0700

    Update problem 02.c

--- theory_assignment/Assignment 02/Problem 02/problem 02.c
+++ theory_assignment/Assignment 02/Problem 02/problem 02.c
@@ -3,3 +3,55 @@
 *  Roll Number: 24K-0640
 *  Problem 02
 */
+
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+
+	int i, j = 0, k, n = 0, l;
+	printf("Slogan: ");
+	char ch[100];
+	int an[j];
+	fgets(ch,100,stdin);
+	int c = strlen(ch);
+	int arr[100][100];
+	int sam[100][256] = {0};
+
+	for(i = 0; i < c - 1;i++){
+		if(ch[i] == ','){
+			an[j] = n;
+			j++;
+			n = 0;
+		}else{
+		arr[j][n++] = ch[i];
+		}//end if
+	}//end for
+	an[j] = n;
+	j++;
+	
+	for(i = 0; i < j;i++){
+		for(k = 0; k < an[i];k++){
+		sam[i][arr[i][k]]++;
+		}//end for
+	}//end for
+	
+	for(k = 0; k < j;k++){
+		printf("For '");
+		for(i = 0; i < an[k];i++){
+			printf("%c", arr[k][i]);
+		}//end for
+		printf("': ");
+		printf("{");
+		for(l = 0; l < an[k];l++){
+			int ci = arr[k][l];
+			if(sam[k][ci] > 0){
+				printf("'%c': %d,", ci, sam[k][ci]);
+				sam[k][ci] = -99;
+			}//end if
+		}//end for
+		printf("}\n ");
+	}//end for
+
+	return 0;
+}//end main
