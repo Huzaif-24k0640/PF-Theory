2639f11864d6f8ee8348af7b48a009c085d5ec2b
Syed Huzaif <syedhuzaifalishah987@gmail.com>
2024-12-07 15:03:38 +0500

    Update problem 10.c

--- theory_assignment/Assignment 02/Problem 10/problem 10.c
+++ theory_assignment/Assignment 02/Problem 10/problem 10.c
@@ -1,57 +1,48 @@
-
 #include<stdio.h>
 
 int main() {
     int num, i, j;
 
     printf("Enter any number: ");
-    scanf("%d", &num);// Input From user
+    scanf("%d", &num);
     
-    /*
-    Printing the Upper part of star
-    */
     for (i = 1; i <= num-1; i++) {
         for (j= 1;  j<= (num-i) * 2; j++) {
-            printf(" ");//Space print
+            printf(" ");
         }
-        printf("*");// Star print
+        printf("*");
         if (i> 1) {
             for (j= 1; j<= (i-1) * 2 - 1; j++) {
-                printf(" ");//Innerspace print
+                printf(" ");
             }
-            printf("*");// Star print
+            printf("*");
         }
         printf("\n");
     }
-    /*
-    Printing the middle line of star
-    */
+
     for (j= 1; j<= num; j++){
-        printf("*");// Star print
+        printf("*");
     }
     for (j= 1; j<= (num-1) * 2 - 1; j++){
-        printf(" ");//Inner space print 
+        printf(" "); 
     }
     for (j= 1; j<= num; j++){
-        printf("*");//star print
+        printf("*");
     }
     printf("\n");
-    /*
-    Printing the lower part of star
-    */
+    
     for (i= num - 1; i>= 1; i--) {
         for (j= 1; j<= (num-i); j++) {
-            printf("  ");//Outer Space
+            printf("  ");
         }
-        printf("*");// Star print
+        printf("*");
         for (j= 1; j<= (i-1) * 2 - 1; j++) {
-            printf(" ");//Inner Space
+            printf(" ");
         }
         if (i> 1) {
-            printf("*");// Star print
+            printf("*");
         }
         printf("\n");
     }
     return 0;
 }
-
